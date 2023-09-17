2cd6138870c5961745b9fdac306ce8a5d949913e
maruf-hussain <engmarufhussain@gmail.com>
2023-09-17 11:15:27 +0600

    sdlfja

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 7000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://maruf11:ggUV2tHxJkLe8E2j@cluster0.l2ejwhb.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const userCollection = client.db("usersDB").collection("users");
+
+
+    app.get('/users', async(req, res)=>{
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const quary = { _id : new ObjectId(id)};
+      const result = await userCollection.deleteOne(quary);
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('CRUD SERVER RUNNING....');
+});
+
+app.listen(port, () => {
+  console.log(`crud server running on port ${port}`)
+})
